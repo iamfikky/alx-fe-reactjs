f787ac348a1940f0759a19e106bd3ab06663e0c2
iamfikky <fikayoobisakin1@gmail.com>
2025-09-03 16:32:42 +0100

    Apply inline CSS styling to components with required values

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile(props) {
   return (
-    <div style={{ border: '1px solid gray', padding: '15px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '15px', borderRadius: '8px', backgroundColor: '#f9f9f9' }}>
       <h2 style={{ color: 'blue', marginBottom: '8px' }}>{props.name}</h2>
       <p style={{ fontSize: '16px' }}>
         Age: <span style={{ fontWeight: 'bold', color: 'darkred' }}>{props.age}</span>
