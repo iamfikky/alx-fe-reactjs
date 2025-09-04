a8b9a94809c5df04003b2f55224f4f9de10d0395
iamfikky <fikayoobisakin1@gmail.com>
2025-09-04 19:09:39 +0100

    Refactor app to use Context API instead of prop drilling for user data

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage userData={userData}/>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// Create a new context for user data
+const UserContext = createContext();
+
+export default UserContext;
