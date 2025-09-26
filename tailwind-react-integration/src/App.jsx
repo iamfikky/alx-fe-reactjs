221ae73e7703797bbac8ac2d7474de304242e801
iamfikky <fikayoobisakin1@gmail.com>
2025-09-26 12:47:00 +0100

    feat: add styled UserProfile component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        🚀 Tailwind is working!
-      </h1>
-    </div>
-  )
+  return <UserProfile />;
 }
 
-export default App
-
+export default App;
